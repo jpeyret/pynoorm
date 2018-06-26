9bee8e060605dac1acd0aa711e0678b4a3394ad6
JL Peyret <jpeyret@gmail.com>
2018-06-26 12:40:58 -0700

    refactored Overrides out of tests

---
+++ pynoorm/overrides.py
@@ -0,0 +1,117 @@
+########### debugging aids ##################
+import pdb
+from traceback import print_exc as xp
+
+def cpdb():
+    """do-nothing debugger test"""
+    return cpdb.enabled
+cpdb.enabled = False
+########### debugging aids ##################
+
+
+class AsObject(object):
+    """used to convert result to objects"""
+    def __init__(self, **kwds):
+        self.__dict__.update(**kwds)
+
+class Overrides(object):
+    def __init__(self, attrnames, type_right=object, to_object_class=None):
+        """prepare a dictionary for linking based on `left_key`
+           :param attrnames:  attributes to set on overrides.  several forms are supported
+           "tax" - a string, will get `tax` the right and assign it to `tax`
+           ("rate","tax") assigns `tax` to `rate` as an alias.
+           ["tax","state"] will get and assign both tax and state.
+           Note that all attributes are expected to exist on the sources, KeyError or AttributeError
+           happen otherwise.
+           :param type_right: by default, attribute retrieval are done by `getattr` because objects
+           are assumed.  passing in `dict` will use `dict[key]` instead.
+           :param to_object_class:  used by `as_objects`.  You can pass in a class but it needs to 
+           support keyword assignment for all attributes' assign aliases.
+
+        """
+
+
+        self.attrnames = attrnames
+        self._dict = {}
+
+        if to_object_class is None:
+            self.to_object_class = AsObject
+        else:
+            self.to_object_class = to_object_class
+
+        if issubclass(type_right, dict):
+            self.setter = self._setter_attr_from_dict
+        else:
+            self.setter = self._setter_attr_from_object
+
+        self._mapping = []
+
+        if not isinstance(attrnames, list):
+            attrnames = [attrnames]
+
+        for attrname in attrnames:
+            if isinstance(attrname, tuple):
+                attrname_l, attrname_r = attrname
+            else:
+                attrname_l = attrname_r = attrname
+            self._mapping.append((attrname_l, attrname_r))            
+
+    def __getitem__(self, attrname):
+        return self._dict[attrname]
+
+    def get(self, key, default=None):
+        """returns an empty dictionary if the key doesnt exist yet"""
+
+        try:
+            return self._dict.setdefault(key, {})
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
+
+
+    def _setter_attr_from_dict(self, o_left, attrnames, o_right):
+        """assigns right-side values to the working dictonary's aliases """
+        try:
+            for attrname_l, attrname_r in self._mapping:
+                o_left[attrname_l] = o_right[attrname_r]
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
+
+    def _setter_attr_from_object(self, o_left, attrname, o_right):
+        """assigns right-side attributes to the working dictonary's aliases """
+        try:
+            for attrname_l, attrname_r in self._mapping:
+                o_left[attrname_l] = getattr(o_right, attrname_r)
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
+
+    def as_dictionaries(self):
+        """return overrides as dictionaries"""
+        try:
+            return self._dict.copy()
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
+
+
+    def as_objects(self): 
+        """return overrides as objects, using `to_object_class`, if provided """
+        try:
+            di = {}
+            cls_ = self.to_object_class
+            for k, v in self._dict.items():
+                di[k] = cls_(**v)
+            return di
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
+
+    def link(self, linker, data):
+        """calls the linker providing itself as the lookup dictionary"""
+        try:
+            linker.link(self, data, attrname_on_left="ignore", setter_left=self.setter)
+        except (Exception,) as e:
+            if cpdb(): pdb.set_trace()
+            raise
--- tests/test_overrides.py
+++ tests/test_overrides.py
@@ -10,7 +10,6 @@ import pprint
 import unittest
 from time import time
 
-from pynoorm.linker import Linker, SlotProxy
 
 import random
 import logging
@@ -32,117 +31,12 @@ def cpdb(e=None):
 cpdb.enabled = False
 ###################################
 
+from pynoorm.linker import Linker
+from pynoorm.overrides import Overrides
 
 
 
 from test_linker import ppp, Customer
-
-class AsObject(object):
-    """used to convert result to objects"""
-    def __init__(self, **kwds):
-        self.__dict__.update(**kwds)
-
-class OverrideManager(object):
-    def __init__(self, attrnames, type_right=object, to_object_class=None):
-        """prepare a dictionary for linking based on `left_key`
-           :param attrnames:  attributes to set on overrides.  several forms are supported
-           "tax" - a string, will get `tax` the right and assign it to `tax`
-           ("rate","tax") assigns `tax` to `rate` as an alias.
-           ["tax","state"] will get and assign both tax and state.
-           Note that all attributes are expected to exist on the sources, KeyError or AttributeError
-           happen otherwise.
-           :param type_right: by default, attribute retrieval are done by `getattr` because objects
-           are assumed.  passing in `dict` will use `dict[key]` instead.
-           :param to_object_class:  used by `as_objects`.  You can pass in a class but it needs to 
-           support keyword assignment for all attributes' assign aliases.
-
-        """
-
-
-        self.attrnames = attrnames
-        self._dict = {}
-
-        if to_object_class is None:
-            self.to_object_class = AsObject
-        else:
-            self.to_object_class = to_object_class
-
-        if issubclass(type_right, dict):
-            self.setter = self._setter_attr_from_dict
-        else:
-            self.setter = self._setter_attr_from_object
-
-        self._mapping = []
-
-        if not isinstance(attrnames, list):
-            attrnames = [attrnames]
-
-        for attrname in attrnames:
-            if isinstance(attrname, tuple):
-                attrname_l, attrname_r = attrname
-            else:
-                attrname_l = attrname_r = attrname
-            self._mapping.append((attrname_l, attrname_r))            
-
-    def __getitem__(self, attrname):
-        return self._dict[attrname]
-
-    def get(self, key, default=None):
-        """returns an empty dictionary if the key doesnt exist yet"""
-
-        try:
-            return self._dict.setdefault(key, {})
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
-
-
-    def _setter_attr_from_dict(self, o_left, attrnames, o_right):
-        """assigns right-side values to the working dictonary's aliases """
-        try:
-            for attrname_l, attrname_r in self._mapping:
-                o_left[attrname_l] = o_right[attrname_r]
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
-
-    def _setter_attr_from_object(self, o_left, attrname, o_right):
-        """assigns right-side attributes to the working dictonary's aliases """
-        try:
-            for attrname_l, attrname_r in self._mapping:
-                o_left[attrname_l] = getattr(o_right, attrname_r)
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
-
-    def as_dictionaries(self):
-        """return overrides as dictionaries"""
-        try:
-            return self._dict.copy()
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
-
-
-    def as_objects(self): 
-        """return overrides as objects, using `to_object_class`, if provided """
-        try:
-            di = {}
-            cls_ = self.to_object_class
-            for k, v in self._dict.items():
-                di[k] = cls_(**v)
-            return di
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
-
-    def link(self, linker, data):
-        """calls the linker providing itself as the lookup dictionary"""
-        try:
-            linker.link(self, data, attrname_on_left="ignore", setter_left=self.setter)
-        except (Exception,) as e:
-            if cpdb(): pdb.set_trace()
-            raise
 
 
 
@@ -181,14 +75,14 @@ class Test_SelfLinker(unittest.TestCase):
             linker = Linker(key_left="custid")
 
             #make a lookup dictionary point to customers by custid
-            lookup = OverrideManager(attrnames="tax")
+            lookup = Overrides(attrnames="tax")
 
             linker.link(lookup, self.customer_rates, attrname_on_left="tax", setter_left=lookup.setter)
             linker.link(lookup, self.customer_rates_overrides, attrname_on_left="tax", setter_left=lookup.setter)
 
             di_data = lookup.as_objects()
 
-            lookup = OverrideManager(attrnames="tax")
+            lookup = Overrides(attrnames="tax")
 
 
             lookup.link(linker, self.customer_rates)
@@ -238,7 +132,7 @@ class Test_SelfLinker(unittest.TestCase):
             linker = Linker(key_left="custid")
 
             #make a lookup dictionary point to customers by custid
-            lookup = OverrideManager(attrnames=["tax","state"])
+            lookup = Overrides(attrnames=["tax","state"])
 
             lookup.link(linker, self.customer_rates)
             lookup.link(linker, self.customer_rates_overrides)
@@ -271,7 +165,7 @@ class Test_SelfLinker(unittest.TestCase):
         """translate right attribute name to left attribute name"""
         try:
             #make a lookup dictionary point to customers by custid
-            lookup = OverrideManager(attrnames=[("rate","tax")])
+            lookup = Overrides(attrnames=[("rate","tax")])
 
             lookup.link(self.linker, self.customer_rates)
             lookup.link(self.linker, self.customer_rates_overrides)
@@ -296,7 +190,7 @@ class Test_SelfLinker(unittest.TestCase):
         try:
             #create the linker and tell it what the left-hand key will be
 
-            lookup = OverrideManager(attrnames="tax", type_right=dict)
+            lookup = Overrides(attrnames="tax", type_right=dict)
 
             lookup.link(self.linker, self.di_customer_rates)
             lookup.link(self.linker, self.di_customer_rates_overrides)
@@ -328,7 +222,7 @@ class Test_SelfLinker(unittest.TestCase):
                     self.tax = tax
                     self.othervalue = othervalue         
 
-            lookup = OverrideManager(attrnames="tax", type_right=dict, to_object_class=CustomObject)
+            lookup = Overrides(attrnames="tax", type_right=dict, to_object_class=CustomObject)
 
             lookup.link(self.linker, self.di_customer_rates)
             lookup.link(self.linker, self.di_customer_rates_overrides)
